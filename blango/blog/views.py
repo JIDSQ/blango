4a47ea133b092cd65d7949b1219e02a5a7fff34f
John Serbeto <john.iszach_serbeto@questronix.com.ph>
2022-09-16 03:21:08 +0000

    Finish caching

--- blango/blog/views.py
+++ blango/blog/views.py
@@ -3,8 +3,12 @@ from django.utils import timezone
 from blog.models import Post
 from blog.forms import CommentForm
 import logging
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_cookie
+
 
 logger = logging.getLogger (__name__)
+
 
 def index(request):
     posts = Post.objects.filter(published_at__lte=timezone.now())
